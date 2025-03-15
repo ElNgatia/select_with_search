f9f55bd68e17f5b69808fcded0f39c5e4cdbf03a
ElNgatia <ngatiadyte@gmail.com>
2025-03-15 19:26:15 +0300

    Update app title and remove commented-out code for FlutterSelect

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Route Management',
+      title: 'Flutter Select',
       theme: ThemeData(primarySwatch: Colors.blue, scaffoldBackgroundColor: Colors.grey[100]),
       home: Scaffold(
         body: Column(
           children: [
-            // MaterialSelect<String>(
-            //   options: const ['Apple', 'Banana', 'Orange', 'Mango'],
-            //   itemBuilder: (value) => Text(value),
-            //   onChanged: (selected) => print(selected),
-            //   multiple: true,
-            //   searchable: false,
-            //   hint: const Text('Select fruits...'),
-            //   popupMaxHeight: 400,
-            //   decoration: const InputDecoration(
-            //     labelText: 'Fruits',
-            //     border: OutlineInputBorder(),
-            //     prefixIcon: Icon(Icons.search),
-            //   ),
-            // ),
             FlutterSelect<String>.withSearch(
               onSearchChanged: (value) {
                 print('Search: $value');
